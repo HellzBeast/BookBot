c0a7eaf1c42465a645031d08d185df10d40938b5
HellzBeast <butchgregor3@gmail.com>
2025-06-11 17:08:58 +0100

    9: added sorting, changed formatting of final report

--- main.py
+++ main.py
@@ -5,11 +5,21 @@ def get_book_text(filepath):
 
 from stats import word_count
 from stats import char_count
+from stats import char_count_sorted
 
 def main():
     book_path = "books/frankenstein.txt"
     content = get_book_text(book_path)
     count = word_count(content)
-    print(f"{count} words found in the document")
-    print(f"Character count: {char_count(content)}")
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {count} total words")
+    print("--------- Character Count -------")
+    for dictionary in char_count_sorted(content):
+        char = dictionary["char"]
+        count = dictionary["num"]
+        if char.isalpha():
+            print(f"{char}: {count}")
+    print("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -8,3 +8,10 @@ def char_count(text):
     for char in lowercase:
         lower_dictionary[char] = lower_dictionary.get(char, 0) + 1
     return lower_dictionary
+
+def char_count_sorted(text):
+    char_list = []
+    for char, count in char_count(text).items():
+        char_list.append({"char": char, "num": count})
+    char_list.sort(key=lambda x: x["num"], reverse=True)
+    return char_list
