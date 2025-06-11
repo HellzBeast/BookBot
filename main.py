3f3fe9e9540521eae560d428e1aace1011674355
HellzBeast <butchgregor3@gmail.com>
2025-06-11 15:53:26 +0100

    6: Filled main.py file with code

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ def get_book_text(filepath):
         text = file.read()
     return text
 
+def word_count(text):
+    words = text.split()
+    return len(words)
+
 def main():
     book_path = "books/frankenstein.txt"
     content = get_book_text(book_path)
-    print(content)
+    count = word_count(content)
+    print(f"{count} words found in the document")
 main()
