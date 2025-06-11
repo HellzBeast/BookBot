747d0a6545da3c859e4a4633e40c44cfb554e0f0
HellzBeast <butchgregor3@gmail.com>
2025-06-11 15:12:46 +0100

    5: added code to main.py file

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as file:
+        text = file.read()
+    return text
+
+def main():
+    book_path = "books/frankenstein.txt"
+    content = get_book_text(book_path)
+    print(content)
